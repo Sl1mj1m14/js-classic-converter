8542ad218cc4e42975e50ac3a6f6caf0399b8288
Sl1mj1m14 <99777514+Sl1mj1m14@users.noreply.github.com>
2025-05-10 11:48:50 -0400

    Updated some initial files

---
+++ src/convert.rs
@@ -0,0 +1,6 @@
+use mc_classic_js::{ChangedBlocks, Data, JSLevel, Settings};
+use mc_classic::Level;
+
+pub fn js_to_classic (data: Data) -> Result<Level,ConversionError> {
+    
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::fs::{self, OpenOptions};
+use std::io::Write;
+use std::process::exit;
+
+use serde::Deserialize;
+use thiserror::Error;
+use rusqlite::Error;
+
+use mc_classic;
+use mc_classic_js;
+
+mod convert;
+
 pub fn main () {
     println!("Hello world!");
 }
