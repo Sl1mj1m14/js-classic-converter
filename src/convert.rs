d0effead5a74eca790ea10b81a11caca05c106b1
Sl1mj1m14 <99777514+Sl1mj1m14@users.noreply.github.com>
2025-05-10 13:40:36 -0400

    Initializing conversion

--- src/convert.rs
+++ src/convert.rs
@@ -1,6 +1,88 @@
+use std::collections::HashMap;
+
+use thiserror::Error;
+
 use mc_classic_js::{ChangedBlocks, Data, JSLevel, Settings};
 use mc_classic::Level;
 
+#[derive(Error, Debug)]
+pub enum ConversionError {}
+
 pub fn js_to_classic (data: Data) -> Result<Level,ConversionError> {
-    
+    let mut level: Level = Level::new();
+
+    //Setting fields
+    println!("Setting fields");
+    level.width = Some(data.js_level.worldSize);
+    level.depth = Some(64);
+    level.height = Some(data.js_level.worldSize);
+    level.creator = Some(data.settings.username);
+
+    //Writing blocks
+    println!("Writing blocks");
+    let mut tile_map: Vec<u8> = mc_classic_js::get_tile_map(data.js_level.worldSize, data.js_level.worldSeed);
+
+    for i in 0..level.depth.unwrap() {
+        for j in 0..level.height.unwrap() {
+            for k in 0..level.width.unwrap() {
+                let key: String = String::from(format!(r#""p{}_{}_{}":"#,k,i,j));
+                if data.js_level.changedBlocks.contains_key(&key) {
+                    tile_map[((i*level.height.unwrap()*level.width.unwrap()) + (j*level.width.unwrap()) + k) as usize] = data.js_level.changedBlocks.get(&key).unwrap().bt;
+                }
+            }
+        }
+    }
+
+    //Converting blocks
+    println!("Converting tile ids");
+    let mut i: usize = 0;
+    for tile in tile_map.clone() {
+        match tile {
+            1 => tile_map[i] = 2, //Grass Block
+            2 => tile_map[i] = 1, //Stone
+            3 => tile_map[i] = 3, //Dirt
+            4 => tile_map[i] = 5, //Planks
+            5 => tile_map[i] = 38, //Rose
+            6 => tile_map[i] = 37, //Dandelion
+            7 => tile_map[i] = 9, //Water
+            8 => tile_map[i] = 6, //Sapling
+            9 => tile_map[i] = 4, //Cobblestone
+            10 => tile_map[i] = 7, //Bedrock
+            11 => tile_map[i] = 12, //Sand
+            12 => tile_map[i] = 13, //Gravel
+            13 => tile_map[i] = 17, //Logs
+            14 => tile_map[i] = 18, //Leaves
+            15 => tile_map[i] = 40, //Red Mushroom
+            16 => tile_map[i] = 39, //Brown Mushroom
+            17 => tile_map[i] = 11, //Lava
+            18 => tile_map[i] = 14, //Gold Ore
+            19 => tile_map[i] = 15, //Iron Ore
+            20 => tile_map[i] = 16, //Coal Ore
+            21 => tile_map[i] = 41, //Block of Gold
+            22 => tile_map[i] = 19, //Sponge
+            23 => tile_map[i] = 20, //Glass
+            24 => tile_map[i] = 21, //Red Cloth
+            25 => tile_map[i] = 22, //Orange Cloth
+            26 => tile_map[i] = 23, //Yellow Cloth
+            27 => tile_map[i] = 24, //Chartreuse Cloth
+            28 => tile_map[i] = 25, //Green Cloth
+            29 => tile_map[i] = 26, //Spring Green Cloth
+            30 => tile_map[i] = 27, //Cyan Cloth
+            31 => tile_map[i] = 28, //Capri Cloth
+            32 => tile_map[i] = 29, //Ultramarine Cloth
+            33 => tile_map[i] = 31, //Violet Cloth
+            34 => tile_map[i] = 30, //Purple Cloth
+            35 => tile_map[i] = 32, //Magenta Cloth
+            36 => tile_map[i] = 33, //Rose Cloth
+            37 => tile_map[i] = 34, //Dark Gray Cloth
+            38 => tile_map[i] = 35, //Light Gray Cloth
+            39 => tile_map[i] = 36, //White Cloth
+            _ => tile_map[i] = 0,
+        }
+        i += 1;
+    }
+
+    level.blocks = Some(tile_map);
+
+    return Ok(level);
 }
--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,151 @@ use thiserror::Error;
 use rusqlite::Error;
 
 use mc_classic;
-use mc_classic_js;
+use mc_classic_js::{self, deserialize_data, JSLevel, Settings, Data};
 
 mod convert;
 
+const INPUT_MODE: u8 = 0;
+const INPUT_FOLDER: &str = "input";
+const INPUT_FILE: &str = "data.sqlite";
+const OUTPUT_FOLDER: &str = "output";
+const OUTPUT_FILE: &str = "level.dat";
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    input_settings: Input,
+    output_settings: Output
+}
+
+#[derive(Deserialize, Debug)]
+struct Input {
+    input_mode: u8,
+    input_folder: String,
+    input_file: String
+}
+
+#[derive(Deserialize, Debug)]
+struct Output {
+    output_folder: String,
+    output_file: String,
+}
+
+#[derive(Error, Debug)]
+pub enum GeneralError {
+    #[error("Error Parsing Config")]
+    TOMLError(#[from] toml::de::Error),
+
+    #[error("File Error")]
+    FileError(#[from] std::io::Error),
+
+    #[error("Classic Error")]
+    ClassicError(#[from] mc_classic::ClassicError),
+
+    #[error("Conversion Error")]
+    ConversionError(#[from] convert::ConversionError),
+
+    #[error("Read Error")]
+    ReadError(#[from] rusqlite::Error),
+
+    #[error("Could not find {0}")]
+    MissingFile(String),
+
+    #[error("Input mode invalid, expected 0 or 1 but found {0}")]   
+    InvalidMode(u8)    
+}
+
 pub fn main () {
-    println!("Hello world!");
+
+    if !fs::exists("config.toml").unwrap() {
+        if let Err(e) = build_settings() {throw(e)}
+    }
+
+    let conf = fs::read_to_string("config.toml").unwrap().replace("-", "_");
+    let config: Config = match toml::from_str(&conf) {
+        Ok(c) => c,
+        Err(e) => {
+            throw(GeneralError::TOMLError(e));
+            exit(1)
+        }
+    };
+
+    if !fs::exists(&config.input_settings.input_folder).unwrap() {
+        if let Err(e) = fs::create_dir(&config.input_settings.input_folder) {throw(GeneralError::FileError(e))}
+    }
+    if !fs::exists(&config.output_settings.output_folder).unwrap() {
+        if let Err(e) = fs::create_dir(&config.output_settings.output_folder) {throw(GeneralError::FileError(e))}
+    }
+
+    println!("Loading level");
+    if !fs::exists(config.input_settings.input_folder.clone() + "/" + &config.input_settings.input_file).unwrap() {
+        throw(GeneralError::MissingFile(config.input_settings.input_folder.clone() + "/" + &config.input_settings.input_file));
+    }
+
+    let mut data: Data = Data::new(JSLevel::default(), Settings::default());
+
+    println!("Reading level");
+    match config.input_settings.input_mode {
+        0 => {
+            let saved_game = match mc_classic_js::read_saved_game(config.input_settings.input_folder.clone() + "/" + &config.input_settings.input_file) {
+                Ok(c) => c,
+                Err(e) => { throw(GeneralError::ReadError(e)); exit(1)}
+            };
+            println!("{:?}", saved_game.len());
+            let settings = match mc_classic_js::read_settings(config.input_settings.input_folder.clone() + "/" + &config.input_settings.input_file) {
+                Ok(c) => c,
+                Err(e) => { throw(GeneralError::ReadError(e)); exit(1)}
+            };
+            data = deserialize_data(saved_game, settings);
+            println!("{:?}", data.js_level.changedBlocks.len());
+        },
+        _ => ()
+    }
+
+    println!("Converting level");
+    let level: mc_classic::Level = match convert::js_to_classic(data) {
+        Ok(c) => c,
+        Err(e) => { throw(GeneralError::ConversionError(e)); exit(1)}
+    };
+
+    println!("Writing level");
+    if let Err(e) = mc_classic::write_level(
+        level, config.output_settings.output_folder + "/" + &config.output_settings.output_file, 1
+    ) {throw(GeneralError::ClassicError(e))};
+
+    println!("Conversion is complete!");
+
+    println!("Press Enter to Exit");
+    let mut s: String = String::from("");
+    std::io::stdin().read_line(&mut s).expect("");
+    return;
+
+}
+
+fn build_settings () -> Result<(),GeneralError>{
+    let mut file = OpenOptions::new()
+    .append(true)
+    .create(true)
+    .open("config.toml").unwrap();
+
+    file.write("[input-settings]\n".as_bytes())?;
+    file.write(format!(r#"input-mode = {INPUT_MODE}"#).as_bytes())?;
+    file.write("\n".as_bytes())?;
+    file.write(format!(r#"input-folder = "{INPUT_FOLDER}""#).as_bytes())?;
+    file.write("\n".as_bytes())?;
+    file.write(format!(r#"input-file = "{INPUT_FILE}""#).as_bytes())?;
+    file.write("\n\n".as_bytes())?;
+    file.write("[output-settings]\n".as_bytes())?;
+    file.write(format!(r#"output-folder = "{OUTPUT_FOLDER}""#).as_bytes())?;
+    file.write("\n".as_bytes())?;
+    file.write(format!(r#"output-file = "{OUTPUT_FILE}""#).as_bytes())?;
+    file.write("\n".as_bytes())?;
+    return Ok(())
+}
+
+fn throw (e: GeneralError) {
+    eprintln!("Error: {:#?}", e);
+    println!("Press Enter to Exit");
+    let mut s: String = String::from("");
+    std::io::stdin().read_line(&mut s).expect("");
+    std::process::exit(1)
 }
